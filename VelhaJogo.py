2a4b8e608216a0a6268b4728cf559dea9032fc51
guigs10mil <gui.m.s@hotmail.com>
2016-04-26 21:37:43 -0300

    Added files via upload
    Adicionamos a opção de reiniciar e implementamos um message box que informa aos jogadores quem ganhou ou se o jogo empatou.

--- VelhaJogo.py
+++ VelhaJogo.py
@@ -1,29 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 25 14:12:31 2016
 
-@author: Guilherme Moraes & Frederico Curti
-"""
-import tkinter as tk
-import VelhaTabu.py
-class Jogo:
-
-    def iniciar_jogo(self):
-        app.iniciar()
-    def vez_jogador(self):
+class Jogao:
+    def __init__(self):
         
-        jogada = 1
-    def contar_jogadaejogador():
-        i = 1
-        while i <= 9:
-            i += 1
-            if i%2 == 0:
-                XO = "O"
-            else:
-                XO = "X"
-                
-jogo = Jogo()
-jogo.iniciar_jogo()
+        self.tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
+        self.horizontal = []
+        self.vertical = []
+        self.diagonal = []
+        self.empat = []
+        self.termino = 0
+        self.jogador = 1
+        self.conta_jogadas = 0
+        
+    def recebe_jogada (self, linha, coluna):
+        self.tabuleiro[linha][coluna] = self.jogador
+        if self.jogador == 1:
+            self.jogador = 10
+        else:
+            self.jogador = 1
+        
+        self.conta_jogadas += 1
+            
+    def verificar(self):
+        for i in range(3):
+            self.horizontal.append(self.tabuleiro[i][0] + self.tabuleiro[i][1] + self.tabuleiro[i][2])
+            self.vertical.append(self.tabuleiro[0][i] + self.tabuleiro[1][i] + self.tabuleiro[2][i])
+        self.diagonal.append(self.tabuleiro[0][0] + self.tabuleiro[1][1] + self.tabuleiro[2][2])    
+        self.diagonal.append(self.tabuleiro[2][0] + self.tabuleiro[1][1] + self.tabuleiro[0][2])
+        
+        for i in self.horizontal:
+            if i == 3 or i == 30:
+                self.termino = 1
+        for i in self.vertical:
+            if i == 3 or i == 30:
+                self.termino = 1
+        for i in self.diagonal:
+            if i == 3 or i == 30:
+                self.termino = 1
+        
 
-jogador1 = X
-jogador2 = O
+        else:
+            self.diagonal = []
+            self.horizontal = []
+            self.vertical = []
+            
+        for i in range(3):
+            for j in range(3):
+                self.empat.append(self.tabuleiro[i][j])
+                
+        
+        
+            
+
+        
--- VelhaTabu.py
+++ VelhaTabu.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from VelhaJogo import *
+from tkinter import messagebox
+
 
 XO = ""
 
@@ -16,13 +18,27 @@ class Tabuleiro:
         self.window.columnconfigure(2, minsize=100, weight=1)       
         self.i = 1
         self.j = "X"
+        self.p1 = 0
+        self.p2 = 0
+#        self.textp1 = "P1: {0}".format(self.p1), fg="blue"
+#        self.textp2 = "P2: {0}".format(self.p2), fg="red"
+        
+         
+            
+        self.criar_botoes()
+        
+#        self.label2 = tk.Label(self.window)
+#        self.label2.configure(text="{0} | {1}".format(self.textp1, self.textp2))
+#        self.label2.grid(row=3, column=2)
+        
+    def criar_botoes(self):
         
         def botao(x, y):
             botao = tk.Button(self.window)
             botao.configure(command=lambda i=x, j=y: self.jogada(i,j))
             botao.configure(text=XO, font='Arial 72')
-            botao.grid(row=x, column=y, sticky="nsew")
-            
+            botao.grid(row=x, column=y, sticky="nsew") 
+        
         self.botoes = []
         for i in range(3):
             linha_botoes = []
@@ -33,6 +49,7 @@ class Tabuleiro:
         self.label = tk.Label(self.window)
         self.label.configure(text="Próxima jogada: {0}".format(self.j))
         self.label.grid(row=3, column=0)
+      
     
     def jogada(self, x, y):
         if self.i % 2 == 0:
@@ -57,7 +74,60 @@ class Tabuleiro:
         J.recebe_jogada(x, y)
         
         J.verificar()
+        
+        if J.termino == 1:
+            self.acabo()
+            
+        soma = 0
+        for i in J.empat:
+            soma += i
+        if soma == 45:
+            self.empate()
+            
+        if J.conta_jogadas == 9:
+            self.empate()
+
+        
+        
+        
+    def acabo(self):
+        
+        if self.j == "X":
+            self.j = "O"
+        else:
+            self.j = "X"
+        
+        if messagebox.askyesno(message="Acabooooooo! O jogador {0} Ganhou!\nDeseja reiniciar?".format(self.j)) == True:
+            self.reiniciar()
+        else:
+            quit()
+    
+    def empate(self):
          
+        if messagebox.askyesno(message="Empatou! Seus ruim!\nDeseja reiniciar?") == True:
+            self.reiniciar()
+        else:
+            quit()
+        
+    def reiniciar(self):
+        
+        self.criar_botoes()
+
+        J.tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
+        
+        J.diagonal = []
+        J.horizontal = []
+        J.vertical = []
+        
+        J.termino = 0
+        
+        J.conta_jogadas = 0
+        
+        self.i = 1
+        self.j = "X"
+        self.label.configure(text="Próxima jogada: {0}".format(self.j))
+        
+        
 
     def iniciar(self):
         self.window.mainloop()
